ba76a4d9ef449088a544eed28720ae0c7514006b
Aldo Souza <aldocnsouza@gmail.com>
2023-09-20 14:20:46 -0300

    ftr: Criação da Interface base

---
+++ main.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+from tkinter import *
+
+app = ctk.CTk()
+app.geometry("1000x600")
+app.title("Bem-vindo ao Avalia Prof!")
+app.resizable(False, False)
+
+app.mainloop()
