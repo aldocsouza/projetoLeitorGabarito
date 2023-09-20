694b2ca5c39b09bf71b329dbb0af27886cf97e40
Aldo Souza <aldocnsouza@gmail.com>
2023-09-20 14:25:09 -0300

    ftr: Criada a Tela a Principal

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ app.geometry("1000x600")
 app.title("Bem-vindo ao Avalia Prof!")
 app.resizable(False, False)
 
+FirstFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
+#TELA PRINCIPAL
+def telaPrincipal():
+    FirstFrameScreen.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
+
+    #CONTEÚDO DO FRAME:
+    lb_title = ctk.CTkLabel(FirstFrameScreen, text="Avalia Prof", font=('Arial', 40, 'bold'), text_color="#1C89DC", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.1)
+
+    lb_inst = ctk.CTkLabel(FirstFrameScreen, text="Selecione as opções:", font=('Arial', 20), text_color="#A5A5A5", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.25)
+
+telaPrincipal()
+
 app.mainloop()
