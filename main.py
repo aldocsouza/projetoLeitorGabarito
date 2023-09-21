e9f2c23ea3f41b9a3ea9cd2a6289a75a52f2201f
Aldo Conceicao Souza <04111799@sempreunama.com.br>
2023-09-21 19:52:09 -0300

    ftr: Adicionado a tela Alunos

--- main.py
+++ main.py
@@ -26,12 +26,14 @@ app._set_appearance_mode('dark')
 FirstFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
 SecondFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
 TerceiroFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
+QuartoFrame = ctk.CTkFrame(app, fg_color="#2D1E1E")
 
 #TELA PRINCIPAL
 def telaPrincipal():
     FirstFrameScreen.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
     SecondFrameScreen.place_forget()
     TerceiroFrameScreen.place_forget()
+    QuartoFrame.place_forget()
 
     #CONTEÚDO DO FRAME:
     lb_title = ctk.CTkLabel(FirstFrameScreen, text="Avalia Prof", font=('Arial', 40, 'bold'), text_color="#1C89DC", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.1)
@@ -64,13 +66,17 @@ def telaCorrigir():
     
     btn_return_CP = ctk.CTkButton(TerceiroFrameScreen, text="Voltar", font=('Arial', 20, 'bold'), command=telaPrincipal).place_configure(relx=0.1, rely=0.12)
 
+#TELA DE ALUNOS
+def telaAlunos():
+    QuartoFrame.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
+    FirstFrameScreen.place_forget()
 
 #BOTÕES TELA PRINCIPAL
 CResposta = ctk.CTkButton(FirstFrameScreen, text="Cartão Resposta", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30, command=telaCartaoResp).place_configure(relx=0.4, rely=0.35, relwidth=0.2, relheight=0.1)
 
 CProva = ctk.CTkButton(FirstFrameScreen, text="Corrigir Prova", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30, command=telaCorrigir).place_configure(relx=0.4, rely=0.475, relwidth=0.2, relheight=0.1)
 
-Alunos = ctk.CTkButton(FirstFrameScreen, text="Alunos", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30).place_configure(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
+Alunos = ctk.CTkButton(FirstFrameScreen, text="Alunos", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30, command=telaAlunos).place_configure(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
 
 GPlanilha = ctk.CTkButton(FirstFrameScreen, text="Gerar Planilha", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30).place_configure(relx=0.4, rely=0.725, relwidth=0.2, relheight=0.1)
 
