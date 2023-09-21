a242b3278de6f3765bb092de87290bfc3866b958
Aldo Souza <aldocnsouza@gmail.com>
2023-09-21 14:24:37 -0300

    ftr: Adicionado a tela Corrigir Prova

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ app._set_appearance_mode('dark')
 #CRIAÇÃO DAS TELAS-FRAMES
 FirstFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
 SecondFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
+TerceiroFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
 
 #TELA PRINCIPAL
 def telaPrincipal():
     FirstFrameScreen.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
     SecondFrameScreen.place_forget()
+    TerceiroFrameScreen.place_forget()
 
     #CONTEÚDO DO FRAME:
     lb_title = ctk.CTkLabel(FirstFrameScreen, text="Avalia Prof", font=('Arial', 40, 'bold'), text_color="#1C89DC", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.1)
@@ -48,11 +50,25 @@ def telaCartaoResp():
     
     btn_return_CR = ctk.CTkButton(SecondFrameScreen, text="Voltar", font=('Arial', 20, 'bold'), command=telaPrincipal).place_configure(relx=0.1, rely=0.12)
 
+#TELA DE CORRIGIR PROVA
+def telaCorrigir():
+    TerceiroFrameScreen.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
+    FirstFrameScreen.place_forget()
+    
+    #CONTEÚDO DO FRAME:
+    CorFrame = ctk.CTkFrame(TerceiroFrameScreen).place_configure(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.6)
+
+    lb_title_CP = ctk.CTkLabel(TerceiroFrameScreen, text="Avalia Prof", font=('Arial', 40, 'bold'), text_color="#1C89DC", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.1)
+
+    lb_subtitle_CP = ctk.CTkLabel(TerceiroFrameScreen, text="Posicione a câmera dentro da marcação", font=('Arial', 20), text_color="#A5A5A5", fg_color="#2D1E1E").place_configure(relx=0.32, rely=0.2)
+    
+    btn_return_CP = ctk.CTkButton(TerceiroFrameScreen, text="Voltar", font=('Arial', 20, 'bold'), command=telaPrincipal).place_configure(relx=0.1, rely=0.12)
+
 
 #BOTÕES TELA PRINCIPAL
 CResposta = ctk.CTkButton(FirstFrameScreen, text="Cartão Resposta", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30, command=telaCartaoResp).place_configure(relx=0.4, rely=0.35, relwidth=0.2, relheight=0.1)
 
-CProva = ctk.CTkButton(FirstFrameScreen, text="Corrigir Prova", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30).place_configure(relx=0.4, rely=0.475, relwidth=0.2, relheight=0.1)
+CProva = ctk.CTkButton(FirstFrameScreen, text="Corrigir Prova", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30, command=telaCorrigir).place_configure(relx=0.4, rely=0.475, relwidth=0.2, relheight=0.1)
 
 Alunos = ctk.CTkButton(FirstFrameScreen, text="Alunos", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30).place_configure(relx=0.4, rely=0.6, relwidth=0.2, relheight=0.1)
 
