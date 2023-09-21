f5b3e843991dd055c7acb52096c93992a3885976
Aldo Souza <aldocnsouza@gmail.com>
2023-09-21 13:51:54 -0300

    ftr: Criada a tela Cartão Reposta

--- main.py
+++ main.py
@@ -6,18 +6,33 @@ app.geometry("1000x600")
 app.title("Bem-vindo ao Avalia Prof!")
 app.resizable(False, False)
 
+#CRIAÇÃO DAS TELAS-FRAMES
 FirstFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
+SecondFrameScreen = ctk.CTkFrame(app, fg_color="#2D1E1E")
+
 #TELA PRINCIPAL
 def telaPrincipal():
     FirstFrameScreen.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
+    SecondFrameScreen.place_forget()
 
     #CONTEÚDO DO FRAME:
     lb_title = ctk.CTkLabel(FirstFrameScreen, text="Avalia Prof", font=('Arial', 40, 'bold'), text_color="#1C89DC", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.1)
 
     lb_inst = ctk.CTkLabel(FirstFrameScreen, text="Selecione as opções:", font=('Arial', 20), text_color="#A5A5A5", fg_color="#2D1E1E").place_configure(relx=0.4, rely=0.25)
 
+#TELA DE CARTÃO RESPOSTA
+def telaCartaoResp():
+    SecondFrameScreen.place_configure(relx=0, rely=0, relwidth=1, relheight=1)
+    FirstFrameScreen.place_forget()
+
+    #CONTEÚDO DO FRAME:
+    CrFrame = ctk.CTkFrame(SecondFrameScreen).place_configure(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.6)
+
+    lb_title_CR = ctk.CTkLabel(SecondFrameScreen, text="Avalia Prof", font=('Arial', 40, 'bold'), text_color="#1C89DC").place_configure(relx=0.4, rely=0.1)
+
+
 #BOTÕES TELA PRINCIPAL
-CResposta = ctk.CTkButton(FirstFrameScreen, text="Cartão Resposta", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30).place_configure(relx=0.4, rely=0.35, relwidth=0.2, relheight=0.1)
+CResposta = ctk.CTkButton(FirstFrameScreen, text="Cartão Resposta", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30, command=telaCartaoResp).place_configure(relx=0.4, rely=0.35, relwidth=0.2, relheight=0.1)
 
 CProva = ctk.CTkButton(FirstFrameScreen, text="Corrigir Prova", font=('Poppins', 20, 'bold'), fg_color="#1C89DC", corner_radius=30).place_configure(relx=0.4, rely=0.475, relwidth=0.2, relheight=0.1)
 
